1abfa01844e992927146741b037dc97a2492037a
Mohit <mohitdynatech@gmail.com>
2026-02-09 14:18:28 +0530

    added test.describe

--- API/Customer_Signup.spec.ts
+++ API/Customer_Signup.spec.ts
@@ -1,28 +1,30 @@
 import * as _ from '@Index';
 
-_.test('POST - Customer Sign Up @api', async ({ request }) => {
+_.test.describe('POST - /auth/signup - @api', async () => {
+    _.test('Customer Sign Up', async ({ request }) => {
 
-    try {
-        const api = new _.Api(request);
-        const user = _.generateRandomUser();
+        try {
+            const api = new _.Api(request);
+            const user = _.generateRandomUser();
 
-        const response = await api.post('/auth/signup', {
+            const response = await api.post('/auth/signup', {
 
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.userEmail,
-            phone: '1234567878',
-            birthDate: '01-01-2000',
-            role: 'customer'
-        });
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.userEmail,
+                phone: '1234567878',
+                birthDate: '01-01-2000',
+                role: 'customer'
+            });
 
-        _.CheckStatus;
-        const body = await _.CheckStatus.ok(response);
-        console.log(body);
-    }
+            _.CheckStatus;
+            const body = await _.CheckStatus.ok(response);
+            console.log(body);
+        }
 
-    catch (error) {
-        console.log("Something Went Wrong.\m", error);
-    }
+        catch (error) {
+            console.log("Something Went Wrong.\m", error);
+        }
 
+    });
 });
